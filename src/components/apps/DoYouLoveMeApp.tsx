46e38fc6902187b65641ad1a0488e85e1702c4fc
Daksh Tomar <123767262+Daksh-Tomar@users.noreply.github.com>
2025-09-18 00:35:08 +0530

    Add files via upload

---
+++ src/components/apps/DoYouLoveMeApp.tsx
@@ -0,0 +1,11 @@
+export const DoYouLoveMeApp = () => {
+  return (
+    <div className="w-full h-full">
+      <iframe
+        src="/do_you_love_me/do_you_love_me.html"
+        title="Do You Love Me"
+        className="w-full h-full rounded-xl border-2 border-pink-300 shadow-lg"
+      />
+    </div>
+  );
+};
